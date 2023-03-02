b0ab30cdd295dbcf9fc38cf8d911e3ef1bb89ee8
jain-neeeraj <31276396+jain-neeeraj@users.noreply.github.com>
2023-03-02 11:48:22 +0530

    reverted to davinci model

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ client = slack.WebClient(token=SLACK_TOKEN)
 
 def generate_text(prompt):
     completions = openai.Completion.create(
-        engine="gpt-3.5-turbo",
+        engine="text-davinci-002",
         prompt=prompt,
         max_tokens=1024,
         n=1,
